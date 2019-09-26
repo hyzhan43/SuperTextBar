bd253902d743c6ad819b1a3c74c148139d94518d
HyZhan <hyzhan43@163.com>
2019-09-26 15:28:40 +0800

    modify super textbar layout

---
+++ core/src/main/java/com/zhan/core/GravityEnum.kt
@@ -0,0 +1,12 @@
+package com.zhan.core
+
+/**
+ *  @author:  hyzhan
+ *  @date:    2019/9/26
+ *  @desc:    TODO
+ */
+enum class GravityEnum(val code: Int) {
+    START(0),
+    CENTER(1),
+    END(2)
+}
--- core/src/main/java/com/zhan/core/SuperTextBar.kt
+++ core/src/main/java/com/zhan/core/SuperTextBar.kt
@@ -4,11 +4,13 @@ import android.content.Context
 import android.graphics.Color
 import android.util.AttributeSet
 import android.util.TypedValue
+import android.view.Gravity
 import android.view.View
 import android.widget.ImageView
 import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.core.view.setPadding
+import com.zhan.ktwing.ext.getColorRef
 import com.zhan.ktwing.ext.gone
 import com.zhan.ktwing.ext.sp2px
 import com.zhan.ktwing.ext.visible
@@ -26,13 +28,16 @@ class SuperTextBar
     defStyle: Int = 0
 ) : RelativeLayout(context, attrs, defStyle) {
 
-    private val smileDrawable = 0
+
     var leftIcon: Int = 0
     var rightIcon: Int = 0
 
     // 默认字体大小
     private val defaultSize = 14f.sp2px
     private val defaultColor = Color.BLACK
+
+    private val defaultDrawable = 0
+    private val defaultLineColor = getColorRef(R.color.grey_400)
 
     var leftText: String = ""
         set(value) {
@@ -54,6 +59,7 @@ class SuperTextBar
         }
     var contentTextSize: Float
     var contentTextColor: Int
+    val contentGravity: Int
 
     var rightText: String = ""
         set(value) {
@@ -79,8 +85,8 @@ class SuperTextBar
         with(typeArray) {
             padding = getDimensionPixelSize(R.styleable.SuperTextBar_padding, 0)
 
-            leftIcon = getResourceId(R.styleable.SuperTextBar_leftIcon, smileDrawable)
-            rightIcon = getResourceId(R.styleable.SuperTextBar_rightIcon, smileDrawable)
+            leftIcon = getResourceId(R.styleable.SuperTextBar_leftIcon, defaultDrawable)
+            rightIcon = getResourceId(R.styleable.SuperTextBar_rightIcon, defaultDrawable)
 
             leftText = getString(R.styleable.SuperTextBar_leftText) ?: ""
             leftTextSize = getDimension(R.styleable.SuperTextBar_leftTextSize, defaultSize)
@@ -89,6 +95,7 @@ class SuperTextBar
             contentText = getString(R.styleable.SuperTextBar_contentText) ?: ""
             contentTextSize = getDimension(R.styleable.SuperTextBar_contentTextSize, defaultSize)
             contentTextColor = getColor(R.styleable.SuperTextBar_contentTextColor, defaultColor)
+            contentGravity = getInt(R.styleable.SuperTextBar_contentGravity, GravityEnum.START.code)
 
             rightText = getString(R.styleable.SuperTextBar_rightText) ?: ""
             rightTextSize = getDimension(R.styleable.SuperTextBar_rightTextSize, defaultSize)
@@ -97,8 +104,8 @@ class SuperTextBar
             showTopLine = getBoolean(R.styleable.SuperTextBar_showTopLine, false)
             showBottomLine = getBoolean(R.styleable.SuperTextBar_showBottomLine, false)
 
-            topLineColor = getColor(R.styleable.SuperTextBar_topLineColor, defaultColor)
-            bottomLineColor = getColor(R.styleable.SuperTextBar_bottomLineColor, defaultColor)
+            topLineColor = getColor(R.styleable.SuperTextBar_topLineColor, defaultLineColor)
+            bottomLineColor = getColor(R.styleable.SuperTextBar_bottomLineColor, defaultLineColor)
 
             recycle()
         }
@@ -115,14 +122,22 @@ class SuperTextBar
         setImageSrc(mIvLeft, leftIcon)
         setImageSrc(mIvRight, rightIcon)
 
-        setTextView(mTvLeft, leftText, leftTextSize, leftTextColor)
-
+        mTvContent.gravity = gainContentGravity()
         setTextView(mTvContent, contentText, contentTextSize, contentTextColor)
-
+        setTextView(mTvLeft, leftText, leftTextSize, leftTextColor)
         setTextView(mTvRight, rightText, rightTextSize, rightTextColor)
 
         mTopLine.apply { if (showTopLine) visible() else gone() }
         mBottomLine.apply { if (showBottomLine) visible() else gone() }
+    }
+
+    private fun gainContentGravity(): Int {
+        return when (contentGravity) {
+            GravityEnum.START.code -> Gravity.START
+            GravityEnum.CENTER.code -> Gravity.CENTER
+            GravityEnum.END.code -> Gravity.END
+            else -> Gravity.START
+        }
     }
 
     private fun setTextView(textView: TextView, content: String, textSize: Float, textColor: Int) {
@@ -141,7 +156,7 @@ class SuperTextBar
     }
 
     private fun setImageSrc(imageView: ImageView, iconRes: Int) {
-        if (iconRes == smileDrawable) {
+        if (iconRes == defaultDrawable) {
             imageView.gone()
             return
         }
